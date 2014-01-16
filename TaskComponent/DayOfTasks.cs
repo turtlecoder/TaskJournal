8cbb33e3d0457c4303c18e026d917cc510364a59
Haroon Khan <haroon.khan@philips.com>
2014-01-16 09:59:23 -0800

    Commiting TaskManagement Project

---
+++ TaskComponent/DayOfTasks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskManagement
+{
+  public class Task
+  {
+    public DateTime StartDate {get;set;}
+    public TimeSpan TimeSpan {get;set;}
+    public string Description {get;set;}
+    public override string ToString()
+    {
+      return new StringBuilder(100)
+             .Append(StartDate.ToString()).Append(",")
+             .Append(TimeSpan.ToString()).Append(",")
+             .Append(Description)
+             .ToString()
+             ;
+    }
+  }
+  public class DayOfTasks
+  {
+    public DateTime DateTime { get; set; }
+    public IEnumerable<Task> TaskList {get;set;}
+    public override string ToString()
+    {
+      return null;
+    }
+  }
+}
